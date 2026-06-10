0281bb702fec2b77687226dfd2c3df541487596f
Dhruvik <81076631+dhruviktank@users.noreply.github.com>
2026-06-10 15:27:02 +0530

    Update directory paths for Kaggle environment

--- code/utils/path.py
+++ code/utils/path.py
@@ -3,9 +3,9 @@
 import os
 
 # CKPT_ROOT = "C:/Users/avarbella/Documents/GraphGym-master/GraphGym-master/graphframex-main/"
-CKPT_ROOT = "C:\\Users\\avarbella\\OneDrive - ETH Zurich\\Documents\\01_GraphGym\\PowerGraph-masterdataset-node\\code\\"
-DATA_DIR = CKPT_ROOT + "dataset\\"
-MODEL_DIR = CKPT_ROOT + "modeltest\\"
-LOG_DIR = CKPT_ROOT + "logs\\"
-RESULT_DIR = CKPT_ROOT + "results\\"
-MASK_DIR = CKPT_ROOT + "mask\\"
+CKPT_ROOT = "/kaggle/working/PowerGraph-node/code/"
+DATA_DIR = CKPT_ROOT + "dataset/"
+MODEL_DIR = CKPT_ROOT + "modeltest/"
+LOG_DIR = CKPT_ROOT + "logs/"
+RESULT_DIR = CKPT_ROOT + "results/"
+MASK_DIR = CKPT_ROOT + "mask/"
